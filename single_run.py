12051985adcf320fe0949d7c3abe5ab4eee10cab
Nicolas Bonnet <mail@nicolasbon.net>
2019-08-17 02:33:53 +0200

    Improve Benchmark
    Discard set-up time

--- single_run.py
+++ single_run.py
@@ -5,27 +5,36 @@ import time
 
 import numpy
 import pathfinder
+from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
+from pathfinding.finder.dijkstra import DijkstraFinder
 
 # Side of the grid.
 x = 100
 y = 100
+p = 0.8
 
 # Generate map.
-obstacles = numpy.zeros((x, y), dtype=bool)
-for i in range(x):
-    for j in range(y):
-        obstacles[i, j] = random.random() > 0.8
+obstacles = numpy.random.choice(a=[False, True], size=(x, y), p=[p, 1 - p])
 
 obstacles[0, 0] = False
 obstacles[x - 1, y - 1] = False
 
-path = pathfinder.dijkstra(obstacles, (0, 0), (x - 1, y - 1))
-start_time = time.time()
 grid = Grid(matrix=numpy.logical_not(obstacles))
+start = grid.node(0, 0)
+end = grid.node(x - 1, y - 1)
+finder = DijkstraFinder(diagonal_movement=DiagonalMovement.always)
+
+start_time = time.time()
+path = pathfinder.dijkstra(obstacles, (0, 0), (x - 1, y - 1))
 t = time.time() - start_time
+
+start_time = time.time()
+finder.find_path(start, end, grid)
+t2 = time.time() - start_time
 
 
 print(grid.grid_str(path=path, start=(0,0), end=(x-1,y-1)))
-print(f"{t*1000} ms")
+print(f"Ext {t*1000:>10f} ms")
+print(f"Lib {t2*1000:>10f} ms")
 
--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 import timeit
 import numpy
 import matplotlib.pyplot as plt
-REPEATS = 500
-NUMBER = 100
-
-setup = """
 import random
 import time
 
@@ -14,51 +10,75 @@ from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.dijkstra import DijkstraFinder
 
+TEST_COUNT = int(1e3)
+t_extension = []
+t_lib_python = []
+
 # Side of the grid.
 x = 100
 y = 100
+p = 0.8
 
-# Generate map.
-obstacles = numpy.zeros((x, y), dtype=bool)
-for i in range(x):
-    for j in range(y):
-        obstacles[i, j] = random.random() > 0.6
-
-obstacles[0, 0] = False
-obstacles[x - 1, y - 1] = False
-
-# Other lib
-grid = Grid(matrix=numpy.logical_not(obstacles))
-start = grid.node(0, 0)
-end = grid.node(x - 1, y - 1)
-finder = DijkstraFinder(diagonal_movement=DiagonalMovement.always)
-"""
-
-stmp_extension = "pathfinder.dijkstra(obstacles, (0, 0), (x - 1, y - 1))"
-stmp_lib_python = "finder.find_path(start, end, grid)"
-
-
-
-t_extension = timeit.repeat(stmp_extension, setup=setup, repeat=REPEATS, number=NUMBER)
-print("Extension   {:>10.3f} milliseconds for {} iterations".format(min(t_extension) * 1000, NUMBER))
-
-t_lib_python = timeit.repeat(stmp_lib_python, setup=setup, repeat=REPEATS, number=NUMBER)
-print("Python lib  {:>10.3f} milliseconds for {} iterations".format(min(t_lib_python) * 1000, NUMBER))
+# Benchmark
+print(f"Running {TEST_COUNT} tests")
 print("----------------------------------")
-print(f"Extension is {numpy.median(t_lib_python) / numpy.median(t_extension)} times faster than python.")
+for i in range(TEST_COUNT):
+    print(f"{i}/{TEST_COUNT}")
+    # Generate map.
+    obstacles = numpy.random.choice(a=[False, True], size=(x, y), p=[p, 1 - p])
 
-t_extension_ms = [t*1000 for t in t_extension]
-t_lib_python_ms = [t*1000 for t in t_lib_python]
+    obstacles[0, 0] = False
+    obstacles[x - 1, y - 1] = False
+
+    # Other lib ini
+    grid = Grid(matrix=numpy.logical_not(obstacles))
+    start = grid.node(0, 0)
+    end = grid.node(x - 1, y - 1)
+    finder = DijkstraFinder(diagonal_movement=DiagonalMovement.always)
+
+    start_time = time.time()
+    path = pathfinder.dijkstra(obstacles, (0, 0), (x - 1, y - 1))
+    ext_time = time.time() - start_time
+
+    start_time = time.time()
+    paths, runs = finder.find_path(start, end, grid)
+    lib_time = time.time() - start_time
+
+    if (paths and path) or (not paths and not path):
+        t_lib_python.append(lib_time)
+        t_extension.append(ext_time)
+    else:
+        raise RuntimeError("Bad state!")
+
+t_extension_ms = [t * 1000 for t in t_extension]
+t_lib_python_ms = [t * 1000 for t in t_lib_python]
+
+print("Median:")
+print("Extension   {:>10.3f} milliseconds".format(numpy.median(t_extension_ms)))
+print("Python lib  {:>10.3f} milliseconds".format(numpy.median(t_lib_python_ms)))
+print("----------------------------------")
+print(f"Extension is {numpy.median(t_lib_python) / numpy.median(t_extension):.1f} times faster than python.")
+
+t_extension_ms.sort()
+t_lib_python_ms.sort()
 
 plt.hist(t_extension_ms, 40, facecolor='g', alpha=0.75)
-plt.xlabel(f'Time [ms] for {NUMBER} executions')
-plt.title(f"CPython extension m={numpy.median(t_extension_ms):.3f} avg={numpy.mean(t_extension_ms):.3f} σ={numpy.nanstd(t_extension_ms):.3f}")
+plt.xlabel(f'Time [ms]')
+plt.title("CPython extension m={:.3f} avg={:.3f} σ = {:.3f}".format(
+    numpy.median(t_extension_ms),
+    numpy.mean(t_extension_ms),
+    numpy.nanstd(t_extension_ms),
+))
 plt.grid(True)
 
 plt.figure()
 plt.hist(t_lib_python_ms, 40, facecolor='r', alpha=0.75)
-plt.xlabel(f'Time [ms] for {NUMBER} executions')
-plt.title(f"Library Pathfinding python m={numpy.median(t_lib_python_ms):.3f} avg={numpy.mean(t_lib_python_ms):.3f} σ={numpy.nanstd(t_lib_python_ms):.3f}")
+plt.xlabel(f'Time [ms]')
+plt.title("Library Pathfinding python m={:.3f} avg={:.3f} σ = {:.3f}".format(
+    numpy.median(t_lib_python_ms),
+    numpy.mean(t_lib_python_ms),
+    numpy.nanstd(t_lib_python_ms),
+))
 plt.grid(True)
 
 plt.show()
